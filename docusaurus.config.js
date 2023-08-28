35f78cdf332f399038a398ced8032d43a9f0eb85
Thomas Gram <thomas@codeshark.net>
2023-08-28 13:58:08 +0200

    Fix casing.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://subnauticanitrox.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/documentation',
+  baseUrl: '/Documentation',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'SubnauticaNitrox', // Usually your GitHub org/user name.
-  projectName: 'documentation', // Usually your repo name.
+  projectName: 'Documentation', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
