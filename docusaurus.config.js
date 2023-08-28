83bb8b8f0e1357b3720284bf1dd0343933e8800e
Thomas Gram <thomas@codeshark.net>
2023-08-28 15:10:27 +0200

    Update title & tagline.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'SubnauticaNitrox Documentation',
-  tagline: 'Dinosaurs are cool',
+  title: 'Nitrox Documentation',
+  tagline: 'Your multiplayer Mod for Subnautica.',
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
